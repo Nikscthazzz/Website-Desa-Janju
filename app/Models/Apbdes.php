1ba7a9612821e804353722e87c9a14c0f193ee6c
sallieeky <sallieeky@gmail.com>
2022-07-26 09:38:39 +0800

    fix model add protected $guarded = ["id"];

--- app/Models/Apbdes.php
+++ app/Models/Apbdes.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Apbdes extends Model
 {
     use HasFactory;
+    protected $guarded = ["id"];
 }
--- app/Models/LayananDesa.php
+++ app/Models/LayananDesa.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class LayananDesa extends Model
 {
     use HasFactory;
+    protected $guarded = ["id"];
 }
