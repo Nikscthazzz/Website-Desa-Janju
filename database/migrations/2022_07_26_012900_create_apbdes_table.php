609d059d852d4537b14036a8b6a1e26339402162
sallieeky <sallieeky@gmail.com>
2022-07-26 09:36:47 +0800

    membuat model tiap table

---
+++ database/migrations/2022_07_26_012900_create_apbdes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apbdes', function (Blueprint $table) {
+            $table->id();
+            $table->string("tahun");
+            $table->string("jenis");
+            $table->string("nama");
+            $table->integer("anggaran");
+            $table->integer("realisasi");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apbdes');
+    }
+};
